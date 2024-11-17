3fd24f86595ba8b6e83e7cd6c9ff7c7a7b8a9a9b
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2024-11-17 17:23:58 -0600

    added Chamber class file and started on init code

---
+++ Chamber.java
@@ -0,0 +1,14 @@
+//Creating a custom class with two field (live and blanks) to store objects of this
+//class in an arraylist
+
+import java.util.ArrayList;
+public class Chamber {
+    int live;
+    int blanks;
+
+    //Constructor
+    public Chamber(int live, int blanks){
+        this.live = live;
+        this.blanks = blanks;
+    }
+}
